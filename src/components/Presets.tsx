672a454351290c166c5ff3bc4102a2b2fd318e90
j- <j@skeoh.com>
2018-11-23 11:35:58 +1100

    Remove presets styling

--- src/components/Presets.tsx
+++ src/components/Presets.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import './Presets.css';
 
 export interface Props extends React.HTMLAttributes<HTMLDivElement> {
 	setControlPoints: (cp1x: number, cp1y: number, cp2x: number, cp2y: number) => void;
@@ -10,39 +9,47 @@ const Presets: React.StatelessComponent<Props> = ({ setControlPoints, ...props }
 		<h2>Presets</h2>
 
 		<button
-			className="Presets-preset btn btn-light"
+			className="btn btn-light btn-lg"
 			type="button"
 			onClick={() => setControlPoints(0, 0, 1, 1)}
 		>
 			linear
 		</button>
 
+		&nbsp;
+
 		<button
-			className="Presets-preset btn btn-light"
+			className="btn btn-light btn-lg"
 			type="button"
 			onClick={() => setControlPoints(0.25, 0.1, 0.25, 1)}
 		>
 			ease
 		</button>
 
+		&nbsp;
+
 		<button
-			className="Presets-preset btn btn-light"
+			className="btn btn-light btn-lg"
 			type="button"
 			onClick={() => setControlPoints(0.42, 0, 1, 1)}
 		>
 			ease-in
 		</button>
 
+		&nbsp;
+
 		<button
-			className="Presets-preset btn btn-light"
+			className="btn btn-light btn-lg"
 			type="button"
 			onClick={() => setControlPoints(0.42, 0, 0.58, 1)}
 		>
 			ease-in-out
 		</button>
 
+		&nbsp;
+
 		<button
-			className="Presets-preset btn btn-light"
+			className="btn btn-light btn-lg"
 			type="button"
 			onClick={() => setControlPoints(0, 0, 0.58, 1)}
 		>
